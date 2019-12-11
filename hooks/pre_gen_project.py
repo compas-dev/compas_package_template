e0225b718faf5648009347f9da97c003cef2d871
Tom Van Mele <vanmelet@ethz.ch>
2019-12-11 10:47:18 +0100

    check for empty name

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -4,7 +4,10 @@ import sys
 MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
 module_name = '{{cookiecutter.project_slug}}'
 
+if not module_name:
+    print('ERROR: The project slug (%s) is not a valid Python module name.' % module_name)
+    sys.exit(1)
+
 if not re.match(MODULE_REGEX, module_name):
     print('ERROR: The project slug (%s) is not a valid Python module name.' % module_name)
-
     sys.exit(1)
