b7234fe7dd5056c1c609371905d038b7b7cbd07b
Chen Kasirer <ckasirer@ethz.ch>
2026-05-15 14:57:54 +0200

    migrated to mkdocs

---
+++ {{cookiecutter.project_folder}}/docs/index.md
@@ -0,0 +1,17 @@
+# {{ cookiecutter.project_name }}
+
+{{ cookiecutter.project_short_description }}
+
+## Installation
+
+Stable releases can be installed from PyPI.
+
+```bash
+pip install {{ cookiecutter.project_slug }}
+```
+
+## First Steps
+
+```python
+import {{ cookiecutter.project_slug }}
+```
--- {{cookiecutter.project_folder}}/tasks.py
+++ {{cookiecutter.project_folder}}/tasks.py
@@ -1,33 +1,25 @@
 import os
 
 from compas_invocations2 import build
-from compas_invocations2 import docs
+from compas_invocations2 import mkdocs
 from compas_invocations2 import style
 from compas_invocations2 import tests
 from invoke.collection import Collection
 
 ns = Collection(
-    docs.help,
     style.check,
     style.lint,
     style.format,
-    docs.docs,
-    docs.linkcheck,
+    mkdocs.docs,
     tests.test,
     tests.testdocs,
     tests.testcodeblocks,
     build.prepare_changelog,
     build.clean,
     build.release,
-    build.build_ghuser_components,
 )
 ns.configure(
     {
         "base_folder": os.path.dirname(__file__),
-        "ghuser": {
-            "source_dir": "src/{{ cookiecutter.project_slug }}_ghpython/components",
-            "target_dir": "src/{{ cookiecutter.project_slug }}_ghpython/components/ghuser",
-            "prefix": "{{ cookiecutter.project_slug }}: ",
-        },
     }
 )
